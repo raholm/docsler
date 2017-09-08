5a5d216657e3aa762ee93833a0c561709aead307
raholm <rasmus.holm723@gmail.com>
2017-09-08 22:55:48 +0200

    R-Cpp sampler interface.

---
+++ docsler/src/R_sampler.cpp
@@ -0,0 +1,34 @@
+#include <Rcpp.h>
+
+#include "def.h"
+#include "util.h"
+#include "document_sampler.h"
+#include "corpus_sampler.h"
+
+using namespace docsler;
+
+// [[Rcpp::export]]
+Rcpp::DataFrame sample_document_cpp(size_t length,
+                                    const Rcpp::NumericVector& topic_probs,
+                                    const Rcpp::NumericMatrix& topic_type_probs) {
+  LdaDocumentSampler sampler{convert_from_R(topic_probs),
+      convert_from_R(topic_type_probs)};
+  Document document = sampler.next(length);
+  IntVector doc_ids(length, document.id);
+  return Rcpp::DataFrame::create(Rcpp::Named("docid") = doc_ids,
+                                 Rcpp::Named("type") = document.types,
+                                 Rcpp::Named("topic") = document.topics);
+}
+
+// [[Rcpp::export]]
+Rcpp::DataFrame sample_corpus_cpp(const Rcpp::IntegerVector length,
+                                  const Rcpp::NumericMatrix& topic_probs,
+                                  const Rcpp::NumericMatrix& topic_type_probs) {
+  LdaCorpusSampler sampler{(size_t) length.size(),
+      convert_from_R(topic_probs),
+      convert_from_R(topic_type_probs)};
+  Corpus corpus = sampler.next(convert_from_R(length));
+  return Rcpp::DataFrame::create(Rcpp::Named("docid") = corpus.doc_ids,
+                                 Rcpp::Named("type") = corpus.types,
+                                 Rcpp::Named("topic") = corpus.topics);
+}
