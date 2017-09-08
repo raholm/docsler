fe40dec7ad6ce10a2820c9c7d2a8b19ef22d8ca2
raholm <rasmus.holm723@gmail.com>
2017-09-08 21:38:18 +0200

    Init package.

---
+++ docsler/R/docsler.R
@@ -0,0 +1,12 @@
+#' docsler R package
+#'
+#' @description
+#' An R package for sampling text documents.
+#'
+#' @useDynLib docsler
+#' @import Rcpp
+#' @importFrom Rcpp evalCpp cpp_object_initializer
+#'
+#' @docType package
+#' @name docsler
+NULL
